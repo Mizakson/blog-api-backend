bec4dee08c16a5ea7bfa2f1e3c7ffd678957392c
Mizakson <mizakson.dev@gmail.com>
2025-11-11 14:20:54 -0500

    Add postSignUp, validateUser to signup.js

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,9 +1,56 @@
 // controllers/signUp.js
 
-// const prisma = require("../prisma/prisma");
+const { validationResult, body } = require("express-validator");
+const prisma = require("../prisma/prisma");
+const bcrypt = require("bcryptjs");
 
 async function getSignUpMessage(req, res) {
     res.status(200).json({ "message": "hi from signup router!" });
 }
 
-module.exports = { getSignUpMessage };
+async function postSignUp(req, res) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.error("Validation failed: ", errors.array());
+
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: errors.array(),
+        });
+    }
+
+    try {
+        const { username, password } = req.body;
+
+        const hashedPw = await bcrypt.hash(password, 10);
+        const newUser = await prisma.user.create({
+            data: {
+                name: username,
+                password: hashedPw,
+            }
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            data: {
+                name: username,
+            }
+        });
+
+    } catch (err) {
+        console.error("User creation error", error);
+        res.status(500).json({
+            message: "An error occured while creating the user",
+            error: error.message,
+        });
+    }
+}
+
+const validateUser = [
+    body("username").isAlpha().notEmpty().withMessage("Please enter a username"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be a minimum of 6 characters"),
+    body("confirmPassword").custom((value, { req }) => value === req.body.password).withMessage("Passwords must match")
+];
+
+module.exports = { getSignUpMessage, postSignUp, validateUser };
