3aadf4230751d45c38af5d22629b3766637d6b4e
Mizakson <mizakson.dev@gmail.com>
2025-11-12 14:23:36 -0500

    Create getSessionStatus method for sessions controller

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,9 +1,46 @@
 // controllers/sessions.js
 
-// const prisma = require("../prisma/prisma");
+const passport = require("passport");
+const prisma = require("../prisma/prisma");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "loremipsum";
 
 async function getSessionStatus(req, res) {
-    res.status(200).json({ "message": "hi from sessions router!" });
+    const currentUser = req.user;
+
+    if (!currentUser || !currentUser.id) {
+        return res.status(401).json({ message: "User not authenticated." });
+    }
+
+    try {
+        const data = await prisma.user.findUnique({
+            where: {
+                id: currentUser.id
+            },
+            include: {
+                posts: {
+                    orderBy: {
+                        id: 'asc'
+                    }
+                }
+            }
+        });
+
+        if (!folders) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.status(200).json({
+            message: 'User data retrieved successfully',
+            user: { id: currentUser.id, name: currentUser.name, role: currentUser.role },
+            data: data,
+        });
+    } catch (error) {
+        console.error("Error in getIndex:", error);
+        next(error);
+    }
+
 }
 
 module.exports = { getSessionStatus };
