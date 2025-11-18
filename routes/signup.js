5fd3e0a0165bd3398969ffc803d86cd818efce47
Mizakson <mizakson.dev@gmail.com>
2025-11-18 14:55:27 -0500

    Add post request to signup router

--- routes/signup.js
+++ routes/signup.js
@@ -6,5 +6,6 @@ const controllers = require("../controllers");
 const signUpRouter = Router();
 
 signUpRouter.get("/", controllers.getSignUpMessage);
+signUpRouter.post("/", controllers.validateUser, controllers.postSignUp);
 
 module.exports = signUpRouter;
