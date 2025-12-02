0f0d0d6b3d5f49d0f8a92e2f58ee0629ddee3271
Mizakson <mizakson.dev@gmail.com>
2025-12-02 11:59:49 -0500

    Edit getUserInfo controller method, add auth middleware to Users router

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,40 @@
 // controllers/users.js
 
-// const prisma = require("../prisma/prisma");
+const prisma = require("../prisma/prisma");
 
 async function getUserMessage(req, res) {
     res.status(200).json({ "message": "hi from users router!" });
 }
 
-async function getUserInfo(req, res) {
-    res.status(200).json({
-        "message": "user info retrieved!",
-        "user-id": req.params.userId
-    });
+async function getUserInfo(req, res, next) {
+
+    const userId = req.params.userId;
+
+    try {
+        const idQuery = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                username: true,
+                role: true
+            }
+        });
+
+        if (idQuery == null) {
+            res.status(404).json({ message: "user not found" });
+        }
+
+        res.status(200).json({
+            "message": "user info retrieved!",
+            "user-info": idQuery
+        });
+    } catch (err) {
+        console.error("error while getting user info: ", err);
+        next(err);
+    }
+
 }
 
 module.exports = { getUserMessage, getUserInfo };
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,12 @@ const { Router } = require("express");
 const controllers = require("../controllers");
 const postsRouter = require("./posts");
 
+const passport = require("passport");
+
 const usersRouter = Router();
 
-usersRouter.get("/", controllers.getUserMessage);
-usersRouter.get("/:userId", controllers.getUserInfo);
+usersRouter.get("/", passport.authenticate('jwt', { session: false }), controllers.getUserMessage);
+usersRouter.get("/:userId", passport.authenticate('jwt', { session: false }), controllers.getUserInfo);
 
 usersRouter.use("/:userId/posts", postsRouter);
 
