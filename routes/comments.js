5bb5634eeb9c8b24d24b5ad81b6c3abc7fa9ce29
Mizakson <mizakson.dev@gmail.com>
2025-12-10 13:38:17 -0500

    Add CRUD routes to commentRouter

--- routes/comments.js
+++ routes/comments.js
@@ -8,4 +8,9 @@ const commentsRouter = Router({ mergeParams: true });
 commentsRouter.get("/", controllers.getCommentMessage);
 commentsRouter.get("/:commentId", controllers.getCommentInfo);
 
+commentsRouter.post("/create-comment", passport.authenticate('jwt', { session: false }), controllers.commentCreate);
+
+commentsRouter.put("/:commentId/update-comment", passport.authenticate('jwt', { session: false }), controllers.updateComment);
+commentsRouter.delete("/:commentId/delete-comment", passport.authenticate('jwt', { session: false }), controllers.deleteComment);
+
 module.exports = commentsRouter;
